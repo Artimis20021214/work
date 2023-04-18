8177d0715546df5e4dc40d71bf85b802bf516b07
Your Name <you@example.com>
2023-04-19 01:15:51 +0800

    Initial commit

--- app/src/main/java/com/example/mobile/MainActivity.kt
+++ app/src/main/java/com/example/mobile/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+    lateinit var txv:TextView
+    var text = "請按"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.text = "請按"
+
+    }
+
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text ="資管2B\n411054113"
+    }
+    fun sad(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text ="張予綸"
+    }
+    fun funny(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text ="我是轉學生來自國立台灣體育大學\n讀了一年轉來這\n個性內向不太愛主動說話\n但我樂意別人跟我聊天"
+    }
+    fun music(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text ="喜好音樂\n唱歌\nbeatbox"
+    }
+
+    override fun onClick(p0: View?) {
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text ="12"
     }
 }
+
+
